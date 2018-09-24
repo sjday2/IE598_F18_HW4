75f75c0fc21e74ef2abfc29b878e2d9d3d86f640
sjday2 <42819585+sjday2@users.noreply.github.com>
2018-09-23 22:05:15 -0500

    revision one

---
+++ IE598_F18_HW4.py
@@ -0,0 +1,205 @@
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from sklearn.datasets.samples_generator import make_regression
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import RANSACRegressor
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+from sklearn import linear_model
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import ElasticNet
+
+
+df = pd.read_csv('https://raw.githubusercontent.com/rasbt/python-machine-learning-book-2nd-edition/master/code/ch10/housing.data.txt', header=None, sep='\s+')
+df.columns = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+df.head()
+
+cols = ['LSTAT', 'INDUS', 'NOX', 'RM', 'MEDV']
+sns.pairplot(df[cols], size=2.5)
+plt.tight_layout()
+plt.show()
+
+cm = np.corrcoef(df[cols].values.T)
+sns.set(font_scale=1.5)
+hm = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size': 15}, yticklabels=cols, xticklabels=cols)
+plt.show()
+
+class LinearRegressionGD(object):
+    def __init__(self, eta=0.001, n_iter=20):
+        self.eta = eta
+        self.n_iter = n_iter
+    def fit(self, X, y):
+        self.w_=np.zeros(1 + X.shape[1])
+        self.cost_ = []
+        
+        for i in range(self.n_iter):
+            output = self.net_input(X)
+            errors = (y - output)
+            self.w_[1:] += self.eta * X.T.dot(errors)
+            self.w_[0] += self.eta * errors.sum()
+            cost = (errors**2).sum() / 2.0
+            self.cost_.append(cost)
+        return self
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:]) +self.w_[0]
+    def predict(self, X):
+        return self.net_input(X)
+    
+X = df[['LSTAT']].values
+y = df['RM'].values
+n_samples = X.shape[0]
+X_train, y_train = X[:n_samples // 2], y[:n_samples // 2]
+X_test, y_test = X[n_samples // 2:], y[n_samples // 2:]
+
+sc_x = StandardScaler()
+sc_y = StandardScaler()
+X_std = sc_x.fit_transform(X)
+y_std = sc_y.fit_transform(y[:, np.newaxis]).flatten()
+lr = LinearRegressionGD()
+lr.fit(X_std, y_std)
+
+sns.reset_orig() # resets matplotlib style
+plt.plot(range(1, lr.n_iter+1), lr.cost_)
+plt.ylabel('SSE')
+plt.xlabel('Epoch')
+plt.show()
+
+def lin_regplot(X, y, model):
+    plt.scatter(X, y, c='steelblue', edgecolor='white', s=70)
+    plt.plot(X, model.predict(X), color='black', lw=2)
+    return None
+
+lin_regplot(X_std, y_std, lr)
+plt.xlabel('LSTAT')
+plt.ylabel('RM')
+plt.show()
+
+#num_rooms_std = sc_x.transform([5.0])
+#price_std = lr.predict(num_rooms_std)
+#print("Price in $1000s: %.3f" %  sc_y.inverse_transform(price_std))
+
+slr = LinearRegression()
+slr.fit(X, y)
+print('Slope: %.3f' % slr.coef_[0])
+print ('Intercept: %.3f' % slr.intercept_)
+
+lin_regplot(X, y, slr)
+plt.xlabel('Average number of rooms [RM]')
+plt.ylabel('Price in $1000s [MEDV]')
+plt.show()
+
+ransac = RANSACRegressor(LinearRegression(), max_trials=100, min_samples=50, loss='absolute_loss', residual_threshold=5.0, random_state=0)
+ransac.fit(X, y)
+
+inlier_mask = ransac.inlier_mask_
+outlier_mask = np.logical_not(inlier_mask)
+line_X = np.arange(3, 10, 1)
+line_y_ransac = ransac.predict(line_X[:, np.newaxis])
+plt.scatter(X[inlier_mask], y[inlier_mask], c='steelblue', edgecolor='white', marker='o', label='Inliers')
+plt.scatter(X[outlier_mask], y[outlier_mask], c='limegreen', edgecolor='white', marker='s', label='Outliers')
+plt.plot(line_X, line_y_ransac, color='black', lw=2)
+plt.xlabel('Average number of rooms [RM]')
+plt.ylabel('Price in $1000s [MEDV]')
+plt.legend(loc='upper left')
+plt.show()
+
+print('Slope: %.3f' % ransac.estimator_.coef_[0])
+print('Intercept: %.3f' % ransac.estimator_.intercept_)
+
+from sklearn.model_selection import train_test_split
+X = df.iloc[:, :-1].values
+y = df['MEDV'].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+slr = LinearRegression()
+slr.fit(X_train, y_train)
+y_train_pred = slr.predict(X_train)
+y_test_pred = slr.predict(X_test)
+
+plt.scatter(y_train_pred,  y_train_pred - y_train, c='steelblue', marker='o', edgecolor='white', label='Training data')
+plt.scatter(y_test_pred,  y_test_pred - y_test, c='limegreen', marker='s', edgecolor='white', label='Test data')
+plt.xlabel('Predicted values')
+plt.ylabel('Residuals')
+plt.legend(loc='upper left')
+plt.hlines(y=0, xmin=-10, xmax=50, color='black', lw=2)
+plt.xlim([-10, 50])
+plt.show()
+
+
+print('MSE train: %.3f, test: %.3f' % (mean_squared_error(y_train, y_train_pred), mean_squared_error(y_test, y_test_pred)))
+
+print('R^2 train: %.3f, test: %.3f' % (r2_score(y_train, y_train_pred), r2_score(y_test, y_test_pred)))
+
+
+#Ridge Regression
+n_alphas = 200
+alphas = np.logspace(-10, -2, n_alphas)
+
+coefs = []
+for a in alphas:
+    ridge = linear_model.Ridge(alpha=a, fit_intercept=False)
+    ridge.fit(X_train, y_train)
+    coefs.append(ridge.coef_)
+    
+ax = plt.gca()
+ax.plot(alphas, coefs)
+ax.set_xscale('log')
+ax.set_xlim(ax.get_xlim()[::-1])
+plt.xlabel('alpha')
+plt.ylabel('weights')
+plt.show()
+
+y_pred_ridge = ridge.fit(X_train, y_train).predict(X_test)
+r2_ridge = r2_score(y_test, y_pred_ridge)
+print("r^2 on test data : %f" % r2_ridge)
+
+##lasso = Lasso(alpha=1.0)
+alpha = 0.1
+lasso = Lasso(alpha=alpha)
+
+y_pred_lasso = lasso.fit(X_train, y_train).predict(X_test)
+r2_score_lasso = r2_score(y_test, y_pred_lasso)
+print(lasso)
+print("r^2 on test data : %f" % r2_score_lasso)
+
+
+##elanet = ElasticNet(alpha=1.0, l1_ratio=0.5)
+enet = ElasticNet(alpha=alpha, l1_ratio=0.95)
+
+y_pred_enet = enet.fit(X_train, y_train).predict(X_test)
+r2_score_enet = r2_score(y_test, y_pred_enet)
+print(enet)
+print("r^2 on test data : %f" % r2_score_enet)
+
+plt.plot(enet.coef_, color='lightgreen', linewidth=2,
+         label='Elastic net coefficients')
+plt.plot(lasso.coef_, color='gold', linewidth=2,
+         label='Lasso coefficients')
+plt.plot(1,'--', color='navy', label='original coefficients')
+plt.legend(loc='best')
+plt.title("Lasso R^2: %f, Elastic Net R^2: %f"
+          % (r2_score_lasso, r2_score_enet))
+plt.show()
+
+print("My name is Stephanie Day")
+print("My NetID is: sjday2")
+print("I hereby certify that I have read the University Policy on Academic Integirty and that I am not in violation.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
